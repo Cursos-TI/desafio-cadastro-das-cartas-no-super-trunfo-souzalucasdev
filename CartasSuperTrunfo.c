d61f71a853d971ee7855c9abe2ba27d81b64831a
Lucas Souza <109295183+souzalucasdev@users.noreply.github.com>
2025-05-15 08:34:57 +0000

    criacao da carta pelo terminal com inputs do usuario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int populacao, pTuristicos, cidade;
+    char nome[30], estado[2];
+    float area, PIB;
+
+    printf("Digite o nome da cidade, sem espacos (ex: SaoJose): \n");
+    scanf("%s", nome);
+
+    printf("Digite o estado (de A a H): \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da cidade de 01 a 04: \n");
+    scanf("%d", &cidade);
+
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite quantos pontos turisticos a cidade tem: \n");
+    scanf("%d", &pTuristicos);
+
+    printf("Digite a area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("%s - %s%d \n", nome, estado, cidade);
+    printf("População: %d \n", populacao);
+    printf("Área: %f \n", area);
+    printf("PIB: %f \n", PIB);
+    printf("Pontos Turísticos: %d \n", pTuristicos);
 
     return 0;
 }
